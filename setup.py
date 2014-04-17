2caa5be7c17f80e9e7a12d1fb8c5daa565520caf
HaiMing.Yin <HaiMing.Yin@gmail.com>
2014-04-17 14:31:56 +0800

    bump version to v0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datafeed',
-    version='0.5',
+    version='0.6',
     author='yinhm',
     author_email='epaulin@gmail.com',    
     packages=['datafeed', 'datafeed/providers', ],
